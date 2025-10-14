8472ebf1579da1d2ea03a56bc65be65e7ace3e02
otumian-empire <popecan1000@gmail.com>
2025-10-14 06:18:59 +0000

    Add dotenv package and configure Vite to use environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import "dotenv/config";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        port: Number(process.env?.PORT ?? 3000),
+    },
+});
